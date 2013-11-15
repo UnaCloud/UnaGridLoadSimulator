414ca6f0f8fee2120c90bea3e47685c3ec7b8bd1
gasotelo <gasotelo@gmail.com>
2013-11-15 09:03:44 -0500

    Minor changes on LoadSimulatorModel.java.

--- src/loadSimulador/LoadSimulatorModel.java
+++ src/loadSimulador/LoadSimulatorModel.java
@@ -7,8 +7,6 @@ import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
